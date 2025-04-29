33a79bbc48e615ff2546438fb797983b21a35729
perepepeu <pedrohenriquerios00@gmail.com>
2025-04-28 21:23:55 -0300

    Adiciona contador, tela de espera e espaço para anúncio

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 import random
 import string
+import os
 
 app = Flask(__name__)
-
 links = {}
 
-def gerar_codigo():
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+def gerar_codigo(tamanho=6):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=tamanho))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    short_link = None
     if request.method == 'POST':
         url_original = request.form['url']
         codigo = gerar_codigo()
         links[codigo] = url_original
-        return render_template('index.html', codigo=codigo)
-    return render_template('index.html')
+        short_link = request.host_url + codigo
+    return render_template('index.html', short_link=short_link)
 
 @app.route('/<codigo>')
 def redirecionar(codigo):
@@ -27,4 +28,5 @@ def redirecionar(codigo):
         return "Link inválido!", 404
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
